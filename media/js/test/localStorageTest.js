14028073b0c9528b7a0744da89feb262f3b95519
Pawelitel <Pawelitel@gmail.com>
2012-10-02 17:10:55 +0400

    добавил test/localStorageTest.js

---
+++ media/js/test/localStorageTest.js
@@ -0,0 +1,12 @@
+﻿function load (w,href)
+{
+	var scriptYN = w.document.createElement("script");
+	scriptYN.type = "text/javascript";
+	scriptYN.async = false;
+	scriptYN.src = href;
+	w.document.getElementsByTagName('head')[0].appendChild(scriptYN);
+}
+load(window,'http://pawelitel.github.com/CDN/javascripts/min/jquery-1.8.2.min.js');
+load(window,'http://pawelitel.github.com/CDN/javascripts/min/breeze.min.js');
+load(window,'http://pawelitel.github.com/CDN/javascripts/min/simpleCookie.min.js');
+
