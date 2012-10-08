6f6929db61ac7eb9281a1960272cba222a2d7354
Pawelitel <Pawelitel@gmail.com>
2012-10-08 21:05:28 +0400

    добавил тестовый юзерскрипт

---
+++ test.user.js
@@ -0,0 +1,15 @@
+﻿var test = <><![CDATA[
+// ==UserScript==
+// @name           test
+// @namespace      test
+// @description    test
+// @include        http://*
+// @include        https://*
+// @include        file:*
+// @copyright      Pawelitel
+// @version        1
+// @license        MTI
+// ==/UserScript==
+]]></>;
+
+alert("begin userscript");
