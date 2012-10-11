725a48bacfb1137980cb2f09252fd3a6d2bdd0d6
Pawelitel <Pawelitel@gmail.com>
2012-10-11 11:14:58 +0400

    отладка harena16mb 1.0.1

--- harena.user.js
+++ harena.user.js
@@ -7,7 +7,7 @@
 // @match			http://*.hockeyarena.net/ru/*
 // @match			http://hockeyarena.net/ru/*
 // @copyright      Pawelitel
-// @version        1
+// @version        1.0.1
 // @license        GNU
 // ==/UserScript==
 
@@ -22,14 +22,15 @@ try {
         return
     }
     if ((w.document.location.href.indexOf("public_league_standings.php") > 0) || (w.document.location.href.indexOf("smenu=mliga") > 0)) {
-        var script = document.createElement('script');
+        var script = w.document.createElement('script');
         script.setAttribute('type', 'text/javascript');
         script.innerHTML = "function harena16mb(){var href = 'http://harena.16mb.com/?';var array = document.getElementsByTagName('table')[1].getElementsByTagName('a');for (var i = 0; i < array.length-1; i++){href+='t'+(i+1)+'='+array[i].href.split('team_id=')[1]+'&';}href+='t'+(i+1)+'='+array[array.length-1].href.split('team_id=')[1];document.location = href;return;};";
-        w.document.getElementsByTagName('head')[0].appendChild(script) var button = document.createElement("input");
+        w.document.getElementsByTagName('head')[0].appendChild(script);
+		var button = w.document.createElement("input");
         button.setAttribute("type", "button");
         button.setAttribute("value", "harena.16mb.com");
         button.setAttribute("onClick", "harena16mb()");
-        w.document.getElementsByTagName("table")[0].getElementsByTagName("td")[0].appendChild(button)
+        w.document.getElementsByTagName("table")[0].getElementsByTagName("td")[0].appendChild(button);
     }
 } catch (e) {
     console.error(e)
