20207ee6ff2c09da476b5041d882e9b16c10eb9c
Pawelitel <Pawelitel@gmail.com>
2012-10-13 17:41:45 +0400

    добавил табличку настроек

--- media/js/pages/mmanazer.js
+++ media/js/pages/mmanazer.js
@@ -1,1 +1,2 @@
-console.log('mmanazer');
+
+$("#page br").eq(0).after('<table width="700" cellpadding="0" cellspacing="0" id="haarp"><tbody><tr class="darkbg"><td class="ysptblhdr" height="18" align="left">HAARP</td></tr></tbody></table><table border="0" width="700" cellspacing="0" cellpadding="2"><tbody></tbody></table>')
