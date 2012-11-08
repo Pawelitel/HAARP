ebb98d98a69496131bc4704fbe462bfadec20f32
Pawelitel <Pawelitel@gmail.com>
2012-11-09 00:22:33 +0400

    отладка Uncaught SyntaxError: Unexpected token <

--- test.user.js
+++ test.user.js
@@ -1,15 +1,17 @@
-var test = <><![CDATA[
 // ==UserScript==
-// @name           test
-// @namespace      test
-// @description    test
-// @include        http://*
-// @include        https://*
-// @include        file:*
-// @copyright      Pawelitel
-// @version        1
-// @license        MTI
+// @run-at		document-end
+// @nocompat
+// @name		test
+// @namespace	test
+// @description	test
+// @version		0.3.2
+// @updateURL   http://pawelitel.github.com/HAARP/test.meta.js
+// @downloadURL	http://pawelitel.github.com/HAARP/test.user.js
+// @author		Pawelitel
+// @copyright   Pawelitel
+// @license     MIT
+// @include     http://*
+// @match		http://*
 // ==/UserScript==
-]]></>;
 
 alert("begin userscript");
